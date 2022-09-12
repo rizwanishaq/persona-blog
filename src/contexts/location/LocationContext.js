16fcc043051c0ec0ed16072771859da0a592fa69
rizwanishaq <rizwanishaq@gmail.com>
2022-09-12 16:34:12 +0200

    modified

--- src/contexts/location/LocationContext.js
+++ src/contexts/location/LocationContext.js
@@ -60,9 +60,7 @@ const LocationContextProvider = ({ children }) => {
   useEffect(() => {
     const getLocation = async () => {
       try {
-        const response = await axios.get(
-          "https://api.bigdatacloud.net/data/reverse-geocode-client"
-        );
+        const response = await axios.get("https://geolocation-db.com/json/");
         const position = response.data;
         success(position);
       } catch (err) {
