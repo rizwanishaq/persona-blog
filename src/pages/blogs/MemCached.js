1e63662a9be33804a4c678649f91125fb338b435
rizwanishaq <rizwanishaq@gmail.com>
2022-12-19 13:07:11 +0100

    memecahded updated with openai

--- src/pages/blogs/MemCached.js
+++ src/pages/blogs/MemCached.js
@@ -8,39 +8,35 @@ const MemCached = () => {
     <Container className="px-xl-0">
       <h1 id="memcached">Memcached</h1>
       <p>
-        I was looking for the redis alternative and found that memcahced is one
-        of the them. So instead of looking other alternative, I just checked the
-        memcahced and feel that it’s a good alternative for simple in-memory key
-        value store operations. And during this researching process I found one
-        very interesting crash course on Memcached architecture{" "}
+        I was searching for alternatives to Redis and discovered that Memcached
+        is a good option for simple in-memory key value store operations. During
+        my research, I came across a helpful crash course on the Memcached
+        architecture by{" "}
         <a href="https://www.youtube.com/watch?v=NCePGsRZFus" target="_blank">
-          video
+          Hussein Nasser
         </a>{" "}
-        from the{" "}
+        on{" "}
         <a
           href="https://www.youtube.com/c/HusseinNasser-software-engineering"
           target="_blank"
         >
-          Hussein Nasser youtube channel
+          YouTube.
         </a>{" "}
-        which explain everything in detail its pros and cons. I highly recommend
-        this{" "}
+        {""}
+        This{" "}
         <a
           href="https://www.youtube.com/c/HusseinNasser-software-engineering"
           target="_blank"
         >
-          channel
-        </a>
-        , you will get lots of information related to software engineering,
-        databases etc. Basically What I am trying to explain in this article is
-        basically taken from that{" "}
-        <a href="https://www.youtube.com/watch?v=NCePGsRZFus" target="_blank">
-          video
-        </a>
-        .
+          {" "}
+          video{" "}
+        </a>{" "}
+        provided detailed information on the pros and cons of Memcached and I
+        highly recommend it for those interested in software engineering and
+        databases.
       </p>
       <p>
-        so first question is what is Memcahced? The answer according to{" "}
+        So, what is Memcached? According to the{" "}
         <a href="https://www.memcached.org/" target="_blank">
           Memcached website
         </a>
@@ -62,53 +58,42 @@ const MemCached = () => {
         </p>
       </blockquote>
       <p>
-        Instead of going into theory about memcached, it is better to watch the{" "}
-        <a href="https://www.youtube.com/watch?v=NCePGsRZFus" target="_blank">
-          video
-        </a>
-        , I will directly implement it in the nodejs, and try to understand how
-        it works with simple example.
-      </p>
-      <p>
-        Before writing the code, we need to start memcached server, and docker
-        is the best option for us to start a simple server. I assume that docker
-        is available on your machine. I am using ubuntu 22.04 and ideally it
-        should work on other operating systems as well.
-      </p>
-      <p>
-        First step is to start server with docker using the following command on
-        terminal
+        It is best to understand Memcached by implementing it in a simple
+        example using Node.js. Before writing the code, we need to start the
+        Memcached server. An easy way to do this is through Docker, which is
+        available on various operating systems including Ubuntu 22.04. To start
+        the server using Docker, run the following command in the terminal:"
       </p>
       <code className="sourceCode bash">
         $ docker run --name memcachedserver -p 11211:11211 -d memcached
       </code>
       <p>
-        and we will have the memcachedserver running on port 11211. We can see
-        that if docker is running or not using the following command
+        This will start the Memcached server on port 11211. To check if Docker
+        is running, use the following command:
       </p>
       <code className="sourceCode bash">$ docker ps</code>
       <p>
-        Now as our memcached server is ready we can use it, and we will access
-        it using nodejs. I assume that you have nodejs installed on your
-        machine.
+        With the Memcached server now running, we can access it using Node.js.
+        If you don't have Node.js installed on your machine, please do so before
+        proceeding.
       </p>
-      <p>we create a folder named MamcachedCrashCourse</p>
+      <p>Let's create a folder called 'MemcachedCrashCourse'</p>
       <code className="sourceCode bash">
         $ mkdir MamcachedCrashCourse <br />$ cd MamcachedCrashCourse
       </code>
       <p>
-        once folder is created, we will run the following command to start our
-        project, and install required packages.
+        After creating the folder, run the following command to start the
+        project and install the necessary packages:
       </p>
       <code className="sourceCode bash">
         $ npm init -y npm install memcached
       </code>
       <p>
-        once packages are installed, we will create a new file with name
-        index.js using following command
+        After installing the necessary packages, create a new file called
+        'index.js' using the following command:
       </p>
       <code className="sourceCode bash">$ touch index.js</code>
-      <p>and write the following lines in index.js</p>
+      <p>In the index.js file, add the following lines of code:</p>
       <Card className="mt-2 mb-4">
         <Card.Body>
           <Card.Text>
@@ -150,23 +135,23 @@ const MemCached = () => {
       </Card>
 
       <p>
-        In the code above we first bring the packages, which are os and
-        memcached, and then we connected to the cluster which is connected to
-        the hostname:11211
+        In the code above, we imported the os and memcached packages and
+        connected to the cluster on hostname:11211
       </p>
       <code className="sourceCode bash">
         const serverPool = new MEMCACHED(os.hostname():11211)
       </code>
       <p>
-        The write function write bar value to the foo key to the memcached
-        server, while read function read the foo key and subsequently we will
-        have bar value. Running the index.js file using the following command
+        The write function writes the value "bar" to the key "foo" in the
+        Memcached server, while the read function retrieves the value "bar"
+        associated with the key "foo". To run the index.js file, use the
+        following command:
       </p>
       <code className="sourceCode bash">$ node index.js</code>
       <p>we will have the output bar.</p>
       <p>
-        Hope this simple example, will show how to use memcached in our nodejs
-        application without any additional configuration.
+        I hope this simple example demonstrates how to use Memcached in a
+        Node.js application without any additional configuration.
       </p>
     </Container>
   );
