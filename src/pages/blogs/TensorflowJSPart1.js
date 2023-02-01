e7823c9a8145aa5c97629b8f77a2013b8f24c5ac
rizwanishaq <rizwanishaq@gmail.com>
2023-02-01 18:55:45 +0100

    tensorflow model added

--- src/pages/blogs/TensorflowJSPart1.js
+++ src/pages/blogs/TensorflowJSPart1.js
@@ -7,11 +7,13 @@ import {
   axisBottom,
   axisLeft,
   scaleOrdinal,
+  shuffle,
 } from "d3";
 import * as tf from "@tensorflow/tfjs";
 import { Button, Container } from "react-bootstrap";
 import DataTable from "react-data-table-component";
 import Accordion from "react-bootstrap/Accordion";
+import LossComponent from "../../components/tensorflowJsExamples/LossComponent";
 
 const DrawTable = ({ data }) => {
   const columns = [
@@ -55,16 +57,16 @@ const DrawTable = ({ data }) => {
   );
 };
 
-const useClassData = ({ data, species }) => {
-  const [classData, setClassData] = useState(null);
-  useEffect(() => {
-    if (data) {
-      const filteredData = data.filter((d) => d.species === species);
-      setClassData(filteredData);
-    }
-  }, [data, species]);
-  return classData;
-};
+// const useClassData = ({ data, species }) => {
+//   const [classData, setClassData] = useState(null);
+//   useEffect(() => {
+//     if (data) {
+//       const filteredData = data.filter((d) => d.species === species);
+//       setClassData(filteredData);
+//     }
+//   }, [data, species]);
+//   return classData;
+// };
 
 const useGetData = () => {
   const [data, setData] = useState(null);
@@ -74,6 +76,8 @@ const useGetData = () => {
         "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/master/inst/extdata/penguins.csv",
         autoType
       );
+      // Randomly shuffle the data
+      shuffle(response_data);
       //   discarding the NA values
       //   https://d3-graph-gallery.com/graph/basic_datamanipulation.html
       const filter_response_data = response_data.filter((d) => {
@@ -92,6 +96,8 @@ const SpeciesDraw = ({ data }) => {
   const margin = { top: 10, right: 30, bottom: 30, left: 60 };
   const width = 460 - margin.left - margin.right;
   const height = 400 - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
   useEffect(() => {
     const svg = select(ref.current)
@@ -101,21 +107,32 @@ const SpeciesDraw = ({ data }) => {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Add X axis
-    const x = scaleLinear().domain([160, 250]).range([0, width]);
+    const x = scaleLinear().domain([160, 250]).range([0, innerWidth]).nice();
     const xAxis = svg
       .append("g")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0," + innerHeight + ")")
       .call(axisBottom(x));
+
     xAxis
       .append("text")
-      .attr("y", 80)
-      .attr("x", width / 2)
+      .attr("class", "axis-label")
+      .attr("y", 40)
+      .attr("x", innerWidth / 2)
       .attr("fill", "black")
-      .text("something");
+      .text("flipper_length_mm ");
 
     // Add Y axis
-    const y = scaleLinear().domain([2500, 7000]).range([height, 0]);
-    svg.append("g").call(axisLeft(y));
+    const y = scaleLinear().domain([2500, 7000]).range([innerHeight, 0]).nice();
+    svg
+      .append("g")
+      .call(axisLeft(y))
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("y", -40)
+      .attr("x", -innerHeight / 4)
+      .attr("fill", "black")
+      .attr("transform", `rotate(-90)`)
+      .text("body_mass_g");
 
     const color = scaleOrdinal()
       .domain(["Adelie", "Gentoo", "Chinstrap"])
@@ -149,13 +166,109 @@ const SpeciesDraw = ({ data }) => {
   );
 };
 
-const TensorflowJSPart1 = () => {
-  const data = useGetData();
-  const species = {
-    Adelie: useClassData({ data: data, species: "Adelie" }),
-    Gentoo: useClassData({ data: data, species: "Gentoo" }),
-    Chinstrap: useClassData({ data: data, species: "Chinstrap" }),
+const useModelData = ({ data }) => {
+  const [xtrain, setXtrain] = useState();
+  const [ytrain, setYtrain] = useState();
+  const [xtest, setXtest] = useState();
+  const [ytest, setYtest] = useState();
+
+  const classes = {
+    Adelie: 0,
+    Chinstrap: 1,
+    Gentoo: 2,
   };
+
+  useEffect(() => {
+    if (data) {
+      const xs = tf.tensor(
+        data.map((x) => [
+          +x.bill_depth_mm,
+          +x.bill_depth_mm,
+          +x.body_mass_g,
+          +x.flipper_length_mm,
+        ])
+      );
+      const ys = tf.cast(
+        tf.tensor(data.map((x) => [+classes[x.species]])),
+        "int32"
+      );
+      const ys_one_hot = tf.squeeze(tf.oneHot(ys, 3), 1);
+      const x_train = xs.slice([1], [300]);
+      const y_train = ys_one_hot.slice([1], [300]);
+      const x_test = xs.slice([300], [-1]);
+      const y_test = ys_one_hot.slice([300], [-1]);
+
+      setXtrain(x_train);
+      setYtrain(y_train);
+      setXtest(x_test);
+      setYtest(y_test);
+    }
+  }, [data]);
+
+  return { xtrain, ytrain, xtest, ytest };
+};
+
+const useModel = () => {
+  const [model, setModel] = useState();
+  useEffect(() => {
+    const model_ = tf.sequential();
+    model_.add(
+      tf.layers.dense({ units: 6, inputShape: [4], activation: "relu" })
+    );
+    model_.add(tf.layers.dense({ units: 3, activation: "softmax" }));
+    model_.compile({
+      optimizer: "sgd",
+      loss: "categoricalCrossentropy",
+      metrics: ["accuracy"],
+    });
+    setModel(model_);
+  }, []);
+
+  return model;
+};
+
+const TrainModel = ({ model, xtrain, ytrain, xtest, ytest }) => {
+  const [loss, setLoss] = useState([]);
+  const [acc, setAcc] = useState([]);
+  const handleClick = async () => {
+    await model.fit(xtrain, ytrain, {
+      batchSize: 4,
+      epochs: 100000,
+      verbose: true,
+      validationSplit: 0.15,
+      callbacks: {
+        onBatchEnd: async (batch, logs) => {
+          //   console.log(batch, logs);
+          //   setLoss((prevloss) => [...prevloss, +logs["loss"].toFixed(2)]);
+          //   setAcc((prevacc) => [...prevacc, +logs["acc"].toFixed(2)]);
+        },
+        onEpochEnd: async (epoch, logs) => {
+          setLoss((prevloss) => [...prevloss, +logs["loss"].toFixed(2)]);
+          setAcc((prevacc) => [...prevacc, +logs["acc"].toFixed(2)]);
+        },
+      },
+    });
+  };
+  return (
+    <Container className="mt-2 mb-2">
+      <LossComponent data={loss} text={"Loss"} />
+      <LossComponent data={acc} text={"Accuracy"} />
+      <Button onClick={handleClick}>Train the model</Button>
+      {/* <Button onClick={handlePredict}>Predict</Button> */}
+    </Container>
+  );
+};
+
+const TensorflowJSPart1 = () => {
+  const model = useModel();
+  const data = useGetData();
+  const { xtrain, ytrain, xtest, ytest } = useModelData({ data });
+  console.log(xtrain && ytrain.shape);
+  // const species = {
+  //   Adelie: useClassData({ data: data, species: "Adelie" }),
+  //   Gentoo: useClassData({ data: data, species: "Gentoo" }),
+  //   Chinstrap: useClassData({ data: data, species: "Chinstrap" }),
+  // };
 
   return (
     <Container className="mt-2">
@@ -169,6 +282,20 @@ const TensorflowJSPart1 = () => {
         <Accordion.Item eventKey="1">
           <Accordion.Header>D3 Graph</Accordion.Header>
           <Accordion.Body>{data && <SpeciesDraw data={data} />}</Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Model</Accordion.Header>
+          <Accordion.Body>
+            {data && (
+              <TrainModel
+                model={model}
+                xtrain={xtrain}
+                ytrain={ytrain}
+                xtest={xtest}
+                ytest={ytest}
+              />
+            )}
+          </Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </Container>
